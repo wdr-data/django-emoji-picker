a287e7eca8d5eeffdb20949408a4324cc0ed2597
Marcus Weiner <marcus.weiner@gmail.com>
2018-05-15 20:11:53 +0000

    :art: Fix missing semicolons

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import { render } from 'react-dom'
-import { Picker } from 'emoji-mart'
+import React from 'react';
+import { render } from 'react-dom';
+import { Picker } from 'emoji-mart';
 
 let panelOpen = false;
 let currentField;
