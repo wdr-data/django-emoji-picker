a40d511af6a2fc91c8d23263783f3584ad810c5e
Jannes Höke <jhoeke@uni-bremen.de>
2018-05-15 19:14:54 +0200

    :tada: Prototype core functionality

---
+++ src/index.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import { render } from 'react-dom'
+import { Picker } from 'emoji-mart'
+
+let panelOpen = false;
+let currentField;
+let currentButton;
+
+const picker = React.createElement(Picker, {
+  set: 'messenger',
+  autofocus: true,
+  onSelect: (emoji) => {
+    if (!currentField) return;
+    console.log(currentField);
+
+    const from = currentField.selectionStart;
+    const to = currentField.selectionEnd;
+
+    currentField.value = currentField.value.substring(0, from) + emoji.native + currentField.value.substring(to)
+  },
+  onClick: (emoji, event) => currentButton.focus(),
+});
+
+const panel = document.createElement('div');
+panel.classList.add('dep-panel');
+
+render(picker, panel);
+document.body.appendChild(panel);
+
+const isInTarget = (elem, target) => elem === target || elem.contains(target);
+
+const clickHandler = (e) => {
+  if (!panelOpen || isInTarget(currentButton, e.target) || isInTarget(panel, e.target)) return;
+  panel.style.display = 'none';
+  panelOpen = false;
+};
+
+document.body.addEventListener('click', clickHandler, true);
+
+document.querySelectorAll('.dep-button').forEach(
+  (elem) => {
+    elem.addEventListener('click', (e) => {
+      const {left, bottom} = e.target.getBoundingClientRect();
+      panel.style.top = `${bottom + 5}px`;
+      panel.style.left = `${left}px`;
+      panel.style.display = 'block';
+
+      setTimeout(() => panelOpen = true);
+      currentField = e.currentTarget.parentNode.firstElementChild;
+      currentButton = e.currentTarget;
+    });
+  }
+);
