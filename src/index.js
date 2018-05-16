e0a74ff6075d4f9a325a8a42d194c60e45f5a050
Marcus Weiner <marcus.weiner@gmail.com>
2018-05-16 08:57:15 +0000

    :bug: Fix js initalization if loaded in head

--- src/index.js
+++ src/index.js
@@ -2,51 +2,53 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Picker } from 'emoji-mart';
 
-let panelOpen = false;
-let currentField;
-let currentButton;
+document.addEventListener('DOMContentLoaded', () => {
+  let panelOpen = false;
+  let currentField;
+  let currentButton;
 
-const picker = React.createElement(Picker, {
-  set: 'emojione',
-  autofocus: true,
-  onSelect: (emoji) => {
-    if (!currentField) return;
+  const picker = React.createElement(Picker, {
+    set: 'emojione',
+    autofocus: true,
+    onSelect: (emoji) => {
+      if (!currentField) return;
 
-    const from = currentField.selectionStart;
-    const to = currentField.selectionEnd;
+      const from = currentField.selectionStart;
+      const to = currentField.selectionEnd;
 
-    currentField.value = currentField.value.substring(0, from) + emoji.native + currentField.value.substring(to)
-  },
-  onClick: (emoji, event) => currentButton.focus(),
+      currentField.value = currentField.value.substring(0, from) + emoji.native + currentField.value.substring(to)
+    },
+    onClick: (emoji, event) => currentButton.focus(),
+  });
+
+  const panel = document.createElement('div');
+  panel.classList.add('dep-panel');
+
+  render(picker, panel);
+  document.body.appendChild(panel);
+
+  const isInTarget = (elem, target) => elem === target || elem.contains(target);
+
+  const clickHandler = (e) => {
+    if (!panelOpen || isInTarget(currentButton, e.target) || isInTarget(panel, e.target)) return;
+    panel.style.display = 'none';
+    panelOpen = false;
+  };
+
+  document.body.addEventListener('click', clickHandler, true);
+
+  document.querySelectorAll('.dep-button').forEach(
+    (elem) => {
+      elem.addEventListener('click', (e) => {
+        const {left, bottom} = e.target.getBoundingClientRect();
+        panel.style.top = `${window.scrollY + bottom + 5}px`;
+        panel.style.left = `${window.scrollX + left}px`;
+        panel.style.display = 'block';
+
+        setTimeout(() => panelOpen = true);
+        currentField = e.currentTarget.parentNode.firstElementChild;
+        currentButton = e.currentTarget;
+      });
+    }
+  );
 });
-
-const panel = document.createElement('div');
-panel.classList.add('dep-panel');
-
-render(picker, panel);
-document.body.appendChild(panel);
-
-const isInTarget = (elem, target) => elem === target || elem.contains(target);
-
-const clickHandler = (e) => {
-  if (!panelOpen || isInTarget(currentButton, e.target) || isInTarget(panel, e.target)) return;
-  panel.style.display = 'none';
-  panelOpen = false;
-};
-
-document.body.addEventListener('click', clickHandler, true);
-
-document.querySelectorAll('.dep-button').forEach(
-  (elem) => {
-    elem.addEventListener('click', (e) => {
-      const {left, bottom} = e.target.getBoundingClientRect();
-      panel.style.top = `${window.scrollY + bottom + 5}px`;
-      panel.style.left = `${window.scrollX + left}px`;
-      panel.style.display = 'block';
-
-      setTimeout(() => panelOpen = true);
-      currentField = e.currentTarget.parentNode.firstElementChild;
-      currentButton = e.currentTarget;
-    });
-  }
-);
