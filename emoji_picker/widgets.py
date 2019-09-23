94072d4c22ae33ec31f9259dde7f9120971195f6
Jannes Höke <mail@jhoeke.de>
2019-09-23 10:53:23 +0200

    :lipstick: Add Admin version of inputs

--- emoji_picker/widgets.py
+++ emoji_picker/widgets.py
@@ -1,31 +1,32 @@
 from django.forms.widgets import TextInput, Textarea
+from django.contrib.admin.widgets import AdminTextInputWidget, AdminTextareaWidget
 
+class MediaClass:
+    js = (
+        'emoji_picker/emoji-picker.js',
+    )
+    css = {
+        'screen': (
+            'emoji_picker/emoji-picker-panel.css',
+            'emoji_picker/emoji-picker-main.css',
+        )
+    }
 
 class EmojiPickerTextInput(TextInput):
-    class Media:
-        js = (
-            'emoji_picker/emoji-picker.js',
-        )
-        css = {
-            'screen': (
-                'emoji_picker/emoji-picker-panel.css',
-                'emoji_picker/emoji-picker-main.css',
-            )
-        }
+    Media = MediaClass
+    template_name = "emoji_picker/textinput.html"
 
+
+class EmojiPickerTextInputAdmin(AdminTextInputWidget):
+    Media = MediaClass
     template_name = "emoji_picker/textinput.html"
 
 
 class EmojiPickerTextarea(Textarea):
-    class Media:
-        js = (
-            'emoji_picker/emoji-picker.js',
-        )
-        css = {
-            'screen': (
-                'emoji_picker/emoji-picker-panel.css',
-                'emoji_picker/emoji-picker-main.css',
-            )
-        }
+    Media = MediaClass
+    template_name = "emoji_picker/textarea.html"
 
+
+class EmojiPickerTextareaAdmin(AdminTextareaWidget):
+    Media = MediaClass
     template_name = "emoji_picker/textarea.html"
